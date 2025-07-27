629f6629b5d518b47855868be7d08383cbfb8ff8
reginaldo ribechi <desafyo@gmail.com>
2025-07-27 00:43:05 +0000

    aumente ao maximo a capacidade de zoom do mapa

--- src/components/map-with-draw.tsx
+++ src/components/map-with-draw.tsx
@@ -41,11 +41,13 @@ const MapWithDraw: React.FC<MapWithDrawProps> = ({ onDrawComplete, onMapStateCha
       const map = L.map(mapContainerRef.current, {
         center: center,
         zoom: zoom,
+        maxZoom: 22,
       });
       mapInstanceRef.current = map;
 
       L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        maxZoom: 20,
       }).addTo(map);
 
       // FeatureGroup to store editable layers
