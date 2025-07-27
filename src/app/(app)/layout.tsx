f5308fe146c60893cbf9397af00195a97c07d53e
reginaldo ribechi <desafyo@gmail.com>
2025-07-27 20:36:11 +0000

    falhou de novo

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -25,7 +25,7 @@ export default function AppLayout({
 
   if (loading || !user) {
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen bg-background">
         <div className="flex items-center gap-2">
           <Leaf className="h-8 w-8 animate-spin text-primary" />
           <p className="text-lg">Carregando...</p>
