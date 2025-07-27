ba4a0d7146fb880cdfd4d24a3b272bec52466ee0
reginaldo ribechi <desafyo@gmail.com>
2025-07-27 01:34:41 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "agrous-iq0pb",
+  "appId": "1:1073025714869:web:d65919dfe0b7c097378cfc",
+  "storageBucket": "agrous-iq0pb.firebasestorage.app",
+  "apiKey": "AIzaSyBjD03BV3-ERs7OXmt50SpkjlN54OGlJ2I",
+  "authDomain": "agrous-iq0pb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1073025714869"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
