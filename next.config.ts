1568a2e0b70bec49b965fc179c05a19fbf13ba15
reginaldo ribechi <desafyo@gmail.com>
2025-07-27 00:08:51 +0000

    ative a funcionalidade de mediçao por poligono e o mapa

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push('node:fs');
+    }
+    return config
+  },
 };
 
 export default nextConfig;
