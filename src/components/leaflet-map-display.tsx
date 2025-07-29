16e1518e24123c7a216a2f4093a2ff1a3959ec65
reginaldo ribechi <desafyo@gmail.com>
2025-07-29 06:14:19 +0000

    ainda nao funcionou, a camada satelite nao esta visivel

--- src/components/leaflet-map-display.tsx
+++ src/components/leaflet-map-display.tsx
@@ -57,8 +57,11 @@ const LeafletMapDisplay: React.FC<LeafletMapDisplayProps> = ({ plots }) => {
                 labelsPane.style.pointerEvents = 'none'; // Clicks go through to the map
             }
 
-            const labelsLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            const labelsLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.png', {
+                attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                subdomains: 'abcd',
+                minZoom: 0,
+                maxZoom: 20,
                 pane: 'labels'
             }).addTo(map);
             
